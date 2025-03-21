f24dfc903eef70c01891381e2390b0cb250793e1
LOmarexe <joneslomar8@gmail.com>
2025-03-21 16:16:36 +0100

    User api(practice#1)

---
+++ models.py
@@ -0,0 +1,14 @@
+class User:
+    def __init__(self, Name, Password, Email):
+        self.id = None
+        self.name = Name
+        self.password = Password
+        self.email = Email
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'password':self.password,
+            'email' : self.email
+        }
+    
