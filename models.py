74f7741cac4c6a66e792367629e68953bb18efc1
LOmarexe <joneslomar8@gmail.com>
2025-03-21 17:16:26 +0100

    User api(practice#1)

--- models.py
+++ models.py
@@ -1,11 +1,6 @@
 class User:
-<<<<<<< HEAD
     def __init__(self, user_id, Name, Password, Email):
-        self.id = user_id
-=======
-    def __init__(self, Name, Password, Email):
         self.id = None
->>>>>>> f24dfc903eef70c01891381e2390b0cb250793e1
         self.name = Name
         self.password = Password
         self.email = Email
